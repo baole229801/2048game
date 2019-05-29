c8d1b22d29a79dc57169ce3687bf22dc041acedb
baoletrg <baole2298@gmail.com>
2019-05-29 21:58:59 +0700

    Add function check cell of Grid

--- grid.js
+++ grid.js
@@ -35,8 +35,41 @@ Grid.prototype.availableCells = function () {
 
 Grid.prototype.randomAvailableCell = function () {
     var cells = this.availableCells();
-  
+
     if (cells.length) {
-      return cells[Math.floor(Math.random() * cells.length)];
+        return cells[Math.floor(Math.random() * cells.length)];
     }
-  };
+};
+
+// Call callback for every cell
+Grid.prototype.eachCell = function (callback) {
+    for (var x = 0; x < this.size; x++) {
+        for (var y = 0; y < this.size; y++) {
+            callback(x, y, this.cells[x][y]);
+        }
+    }
+};
+
+// Check if there are any cells available
+Grid.prototype.cellsAvailable = function () {
+    return !!this.availableCells().length;
+};
+
+// Check if the specified cell is taken
+Grid.prototype.cellAvailable = function (cell) {
+    return !this.cellOccupied(cell);
+};
+
+Grid.prototype.cellOccupied = function (cell) {
+    return !!this.cellContent(cell);
+};
+
+Grid.prototype.cellContent = function (cell) {
+    if (this.withinBounds(cell)) {
+        return this.cells[cell.x][cell.y];
+    } else {
+        return null;
+    }
+};
+
+module.export = Grid;
